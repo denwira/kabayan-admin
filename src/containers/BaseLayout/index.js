6619a93b07111acded7c3695ac6615637f05cb0a
denwira <denrwijaya@gmail.com>
2020-03-08 13:50:05 +0700

    Added base layout

--- src/containers/BaseLayout/index.js
+++ src/containers/BaseLayout/index.js
@@ -1,1 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+
+class BaseLayout extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header className="header-wrap">
+          <h1>{this.props.headerTitle}</h1>
+        </header>
+        <section id="main-section" className="section-container">
+          <div className="main-section-wrapper">
+            <main className="main-section-content">{this.props.children}</main>
+            <aside className="main-section-sidebar">
+              <h1>Sidebar</h1>
+            </aside>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default BaseLayout;
