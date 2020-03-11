8e1a74d2c41e2899c8defe104b2b5aa890e5bcbb
denwira <denrwijaya@gmail.com>
2020-03-11 14:51:08 +0700

    add BaseHeader

---
+++ src/containers/BaseHeader/index.js
@@ -0,0 +1,10 @@
+import { React, Component } from "libraries";
+import { Section } from "components";
+
+class BaseHeader extends Component {
+  render() {
+    return <Section id=""></Section>;
+  }
+}
+
+export default BaseHeader;
