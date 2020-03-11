74fc0195689b87f0393bfaf5ebb37e97b135d5d9
denwira <denrwijaya@gmail.com>
2020-03-11 15:38:48 +0700

    update

--- src/containers/BaseHeader/index.js
+++ src/containers/BaseHeader/index.js
@@ -1,9 +1,22 @@
 import { React, Component } from "libraries";
-import { Section } from "components";
+import { Section, View } from "components";
 
 class BaseHeader extends Component {
   render() {
-    return <Section id=""></Section>;
+    return (
+      <Section id="header" className="header">
+        <View className="header__inner">
+          <View className="header__inner--flex row">
+            <View className="header__item">
+              <h1>Kiri</h1>
+            </View>
+            <View className="header__item">
+              <h1>Kanan</h1>
+            </View>
+          </View>
+        </View>
+      </Section>
+    );
   }
 }
 
